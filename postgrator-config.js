a7fbc2b619c3ad3e7d69e526ae8a781d3e24051d
James Lee <jameswlee109@gmail.com>
2019-12-20 17:30:14 -0600

    deploy db

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,7 +4,7 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATBASE_URL
+    ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
   "host": process.env.MIGRATION_DATABASE_HOST,
   "port": process.env.MIGRATION_DATABASE_PORT,
--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,7 @@ process.env.JWT_SECRET = 'test-jwt-secret'
 
 require('dotenv').config()
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
   || "postgresql://james@localhost/trippie_test"
 
 const { expect } = require('chai')
